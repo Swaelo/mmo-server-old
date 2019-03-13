63063d6fbd8f6fe57e861167bc86e302a48c4e1b
Swaelo <harleylaurie@rocketmail.com>
2019-03-14 00:14:49 +1000

    added file header, updated use of the new NavMesh class and added F2 keyboard event telling the current enemy to compute a pathway to the FPS controllers current location and traverse that pathway to the target location

--- Physics/WorldSimulator.cs
+++ Physics/WorldSimulator.cs
@@ -1,4 +1,9 @@
-﻿using System;
+﻿// ================================================================================================================================
+// File:        WorldSimulator.cs
+// Description: Handles all the server side physics simulation / game logic while the server is running
+// ================================================================================================================================
+
+using System;
 using System.Diagnostics;
 using BEPUphysics;
 using BEPUutilities;
@@ -8,13 +13,20 @@ namespace Server.Physics
 {
     public static class WorldSimulator
     {
-        private static Rendering.Window Window;
+        private static Rendering.Window Window;     //monogame window used to render whats going on with the server
         private static int AccumulatedPhysicsFrames;
         private static double AccumulatedPhysicsTime;
         private static double PreviousTimeMeasurement;
-        private static ParallelLooper ParallelLooper;
+        private static ParallelLooper ParallelLooper;   //assigns however many system threads are available so they can be used by the server to perform jobs
         public static double PhysicsTime { get; private set; }
         public static Space Space;
+        
+        public static Pathfinding.NavMesh TestLevelNavMesh; //Navigation mesh used for AI pathfinding
+        public static Data.TerrainMesh LevelMesh;   //Level collision mesh used for physics
+
+        public static Entities.EnemyEntity Enemy;   //test enemy
+        
+        public static Rendering.FPSCharacterControls FPSController; //FPS controller which contains fps camera inside it to move around the server with
 
         public static void InitializeSimulation(Rendering.Window GameWindow)
         {
@@ -32,26 +44,40 @@ namespace Server.Physics
             Space = new Space(ParallelLooper);
             Space.ForceUpdater.Gravity = new Vector3(0, -9.81f, 0);
 
-            //Load in the physics for the world terrain mesh
-            Data.TerrainMesh LevelTerrain = new Data.TerrainMesh("TerrainMesh", GameWindow);
-            GameWindow.ModelDrawer.Add(LevelTerrain.MeshCollider);
+            //Position the scene camera then attach it to the fps controller
+            Rendering.Window.Instance.Camera.Position = new Vector3(-3.49f, 5.14f, 4.50f);
+            Rendering.Window.Instance.Camera.ViewDirection = new Vector3(0.92f, -0.16f, -0.33f);
+            FPSController = new Rendering.FPSCharacterControls(Space, Rendering.Window.Instance.Camera, Rendering.Window.Instance);
+            FPSController.CharacterController.Body.Tag = "noDisplayObject";
 
-            //Load in the AI navigation mesh
-            Pathfinding.NavMesh NavigationMesh = new Pathfinding.NavMesh("NavMesh");
-            //GameWindow.ModelDrawer.Add(NavigationMesh.MeshData);
+            //Load the level navigation mesh
+            TestLevelNavMesh = new Pathfinding.NavMesh("AITestLevelNavMesh");
+            Rendering.Window.Instance.ModelDrawer.Add(TestLevelNavMesh.MeshData);
 
-            //Place an enemy entity into the game world
-            Entities.EnemyEntity SkeletonWarrior = new Entities.EnemyEntity("Skeleton Warrior", new Vector3(-65.96f, 18.24f, -32.09f));
+            //Load the level collision mesh
+            LevelMesh = new Data.TerrainMesh("AITestLevelCollision");
+            GameWindow.ModelDrawer.Add(LevelMesh.MeshCollider);
+
+            //Place an enemy entity into the level
+            Enemy = new Entities.EnemyEntity(new Vector3(19.31f, 1.64f, 0.44f));
         }
 
         public static void Update(float DeltaTime)
         {
+            //Log what time this update function started
+            long UpdateStart = Stopwatch.GetTimestamp();
+            
+            //Update all the entities AI which are active in the game world right now
             Entities.EntityManager.UpdateEntities(DeltaTime);
             
-            long UpdateStart = Stopwatch.GetTimestamp();
-
+            //Update the physics scene
             Space.Update();
 
+            //update the camera and debug character controller
+            Rendering.Window Game = Rendering.Window.Instance;
+            FPSController.Update(DeltaTime, Game.PreviousKeyboardInput, Game.KeyboardInput);
+
+            //Calculate the physics time values based on how long this frame update took
             long UpdateEnd = Stopwatch.GetTimestamp();
             AccumulatedPhysicsTime += (UpdateEnd - UpdateStart) / (double)Stopwatch.Frequency;
             AccumulatedPhysicsFrames++;
@@ -63,8 +89,6 @@ namespace Server.Physics
                 AccumulatedPhysicsTime = 0;
                 AccumulatedPhysicsFrames = 0;
             }
-
-            Window.Camera.Update(DeltaTime);
         }
 
         public static void CleanUp()
